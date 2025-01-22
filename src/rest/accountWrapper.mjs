8c8214f0f9b8de0c4722de2f7c6377a1a05546da
dutu <dutu@protonmail.com>
2025-01-22 08:42:20 +0200

    Rename account methods and updated endpoints to better align with their functionality
    Signed-off-by: dutu <dutu@protonmail.com>

--- src/rest/accountWrapper.mjs
+++ src/rest/accountWrapper.mjs
@@ -15,6 +15,26 @@ export class AccountWrapper extends BaseWrapper {
     })
   }
 
+  getApikeyInfo(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/user/api-key',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getAccountType (params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/hf/accounts/opened',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
   getAccountList(params) {
     return this.makeRequest({
       endpoint: '/api/v1/accounts',
@@ -28,16 +48,6 @@ export class AccountWrapper extends BaseWrapper {
   getAccountDetail(params) {
     return this.makeRequest({
       endpoint: '/api/v1/accounts/{accountId}',
-      method: 'GET',
-      requiresAuth: true,
-      baseUrl: 'spot',
-      params,
-    })
-  }
-
-  getAccountDetailMargin(params) {
-    return this.makeRequest({
-      endpoint: '/api/v1/margin/account',
       method: 'GET',
       requiresAuth: true,
       baseUrl: 'spot',
@@ -110,32 +120,12 @@ export class AccountWrapper extends BaseWrapper {
       endpoint: '/api/v1/transaction-history',
       method: 'GET',
       requiresAuth: true,
-      baseUrl: 'spot',
+      baseUrl: 'futures',
       params,
     })
   }
 
-  getAllSubAccountsInfo(params) {
-    return this.makeRequest({
-      endpoint: '/api/v1/sub/user',
-      method: 'GET',
-      requiresAuth: true,
-      baseUrl: 'spot',
-      params,
-    })
-  }
-
-  getAllSubAccountsInfoV2(params) {
-    return this.makeRequest({
-      endpoint: '/api/v2/sub/user',
-      method: 'GET',
-      requiresAuth: true,
-      baseUrl: 'spot',
-      params,
-    })
-  }
-
-  createSubAccount(params) {
+  addSubAccount(params) {
     return this.makeRequest({
       endpoint: '/api/v2/sub/user/created',
       method: 'POST',
@@ -145,7 +135,38 @@ export class AccountWrapper extends BaseWrapper {
     })
   }
 
-  getSubAccountBalance(params) {
+  addSubAccountMarginPermission(params) {
+    return this.makeRequest({
+      endpoint: '/api/v3/sub/user/margin/enable',
+      method: 'POST',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  addSubAccountFuturesPermission(params) {
+    return this.makeRequest({
+      endpoint: '/api/v3/sub/user/futures/enable',
+      method: 'POST',
+      requiresAuth: true,
+      baseUrl: 'futures',
+      params,
+    })
+  }
+
+
+  getSubAccountListSummaryInfo(params) {
+    return this.makeRequest({
+      endpoint: '/api/v2/sub/user',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getSubAccountDetailBalance(params) {
     return this.makeRequest({
       endpoint: '/api/v1/sub-accounts/{subUserId}',
       method: 'POST',
@@ -155,17 +176,7 @@ export class AccountWrapper extends BaseWrapper {
     })
   }
 
-  getAllSubAccountsBalance(params) {
-    return this.makeRequest({
-      endpoint: '/api/v1/sub-accounts',
-      method: 'GET',
-      requiresAuth: true,
-      baseUrl: 'spot',
-      params,
-    })
-  }
-
-  getAllSubAccountsBalanceV2(params) {
+  getSubAccountListSpotBalance(params) {
     return this.makeRequest({
       endpoint: '/api/v2/sub-accounts',
       method: 'GET',
@@ -175,10 +186,30 @@ export class AccountWrapper extends BaseWrapper {
     })
   }
 
-  getAllSubAccountsFutures(params) {
+  getSubAccountListFuturesBalance(params) {
     return this.makeRequest({
       endpoint: '/api/v1/account-overview-all',
       method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'futures',
+      params,
+    })
+  }
+
+  addSubAccountApi(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/sub/api-key',
+      method: 'POST',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  modifySubAccountApi(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/sub/api-key/update',
+      method: 'POST',
       requiresAuth: true,
       baseUrl: 'spot',
       params,
@@ -195,6 +226,106 @@ export class AccountWrapper extends BaseWrapper {
     })
   }
 
+  deleteSubAccountApi(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/sub/api-keye',
+      method: 'DELETE',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  addDepositAddress(params) {
+    return this.makeRequest({
+      endpoint: '/api/v3/deposit-address/create',
+      method: 'POST',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getDepositAddress(params) {
+    return this.makeRequest({
+      endpoint: '/api/v3/deposit-address',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getDepositHistory(params) {
+    return this.makeRequest({
+      endpoint: '/api/v3/deposits',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getWithdrawalQuotas(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/withdrawals/quotas',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  addWithdraw(params) {
+    return this.makeRequest({
+      endpoint: '/api/v3/withdrawals',
+      method: 'POST',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  cancelWithdraw(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/withdrawals/{withdrawalId}',
+      method: 'DELETE',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getWithdrawalHistory(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/withdrawals',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getTransferQuotas(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/accounts/transferable',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
+  addFlexTransfer(params) {
+    return this.makeRequest({
+      endpoint: '/api/v3/accounts/universal-transfer',
+      method: 'POST',
+      requiresAuth: true,
+      baseUrl: 'spot',
+      params,
+    })
+  }
+
   getBaseUserFee(params) {
     return this.makeRequest({
       endpoint: '/api/v1/base-fee',
@@ -205,12 +336,22 @@ export class AccountWrapper extends BaseWrapper {
     })
   }
 
-  getTradingPairsFee(params) {
+  getActualFee(params) {
     return this.makeRequest({
       endpoint: '/api/v1/trade-fees',
       method: 'GET',
       requiresAuth: true,
       baseUrl: 'spot',
+      params,
+    })
+  }
+
+  getActualFeeFutures(params) {
+    return this.makeRequest({
+      endpoint: '/api/v1/trade-fees',
+      method: 'GET',
+      requiresAuth: true,
+      baseUrl: 'futures',
       params,
     })
   }
